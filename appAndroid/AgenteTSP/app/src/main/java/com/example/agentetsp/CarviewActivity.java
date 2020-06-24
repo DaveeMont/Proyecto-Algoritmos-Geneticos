54563a8ae903b56dcd0f9aee2d1305a18d9bf62e
Ernesto-hu97 <huitziltneto97@gmail.com>
2020-06-24 00:38:24 -0500

    Añadiendo activities

---
+++ appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/CarviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.agentetsp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carview);
+    }
+}
