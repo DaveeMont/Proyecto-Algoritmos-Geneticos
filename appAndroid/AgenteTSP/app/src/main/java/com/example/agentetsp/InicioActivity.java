e44439ec82b2e466e62b44a5cd9b7944b7a1942a
Ernesto-hu97 <huitziltneto97@gmail.com>
2020-06-24 01:43:47 -0500

    cambio botones

--- appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/InicioActivity.java
+++ appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/InicioActivity.java
@@ -20,4 +20,10 @@ public class InicioActivity extends AppCompatActivity {
         Intent mainn= new Intent(this, MainActivity.class);
         startActivity(mainn);
     }
+
+    //Método del boton guardar
+
+    public void Guardar(View view){
+
+    }
 }
