8d8b4b4950c7b65451e13d5bae6468236db68ec4
MarqCervMartin <martin_mcervantes@hotmail.com>
2020-06-24 01:54:14 -0500

    Probando sincronizacion

--- appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/InicioActivity.java
+++ appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/InicioActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InicioActivity extends AppCompatActivity {
 
 
+    Button btnGuardar;
+    EditText edtAdress;
+    TextView txtCoord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        btnGuardar = (Button) findViewById(R.id.button3);
     }
 
     //Método para el boton anterior
