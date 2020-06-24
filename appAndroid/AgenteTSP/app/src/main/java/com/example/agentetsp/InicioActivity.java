a50c878b829bc0e99fbed46cc2d06564a2a7e802
MarqCervMartin <martin_mcervantes@hotmail.com>
2020-06-24 01:30:18 -0500

    Añadi clase manejador web services

--- appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/InicioActivity.java
+++ appAndroid/AgenteTSP/app/src/main/java/com/example/agentetsp/InicioActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class InicioActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
